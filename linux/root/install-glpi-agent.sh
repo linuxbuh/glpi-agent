45edd3a37b570679dd47b0af54186b4c8206e739
linuxbuh <linuxbuh@yandex.ru>
2022-08-23 16:40:15 +0300

    Update install-glpi-agent.sh

--- linux/root/install-glpi-agent.sh
+++ linux/root/install-glpi-agent.sh
@@ -4,7 +4,7 @@ cd /root
 
 wget -O /root/glpi-agent-1.5-git5c04df6a-linux-installer.pl https://github.com/linuxbuh/glpi-agent/raw/main/linux/glpi-agent-1.5-git5c04df6a-linux-installer.pl
 
-perl /root/glpi-agent-1.5-git5c04df6a-linux-installer.pl --type=all
+perl /root/glpi-agent-1.5-git5c04df6a-linux-installer.pl --install --type=all --service --color --server=https://glpi.ztime.ru/front/inventory.php --logger=file --logfile=/var/log/glpi-agent.log --no-ssl-check --runnow
 
 rm /root/glpi-agent-1.5-git5c04df6a-linux-installer.pl
 
@@ -14,9 +14,9 @@ yum install git
 
 git clone https://github.com/linuxbuh/glpi-agent.git
 
-cp -f /root/glpi-agent/linux/etc/glpi-agent/agent.cfg /etc/glpi-agent/agent.cfg
+#cp -f /root/glpi-agent/linux/etc/glpi-agent/agent.cfg /etc/glpi-agent/agent.cfg
 
-rm -f /etc/glpi-agent/conf.d/00-install.cfg
+#rm -f /etc/glpi-agent/conf.d/00-install.cfg
 
 service glpi-agent restart
 
