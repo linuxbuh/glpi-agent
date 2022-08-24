1d807fa8653e04375603e462debe5f7c41aea953
linuxbuh <linuxbuh@yandex.ru>
2022-08-24 11:32:03 +0300

    Create glpi-setup-win7-64-stable.bat

---
+++ windows/glpi-setup-win7-64-stable.bat
@@ -0,0 +1,5 @@
+C:\wget.exe -O C:\GLPI-Agent-1.4-x64.msi https://github.com/glpi-project/glpi-agent/releases/download/1.4/GLPI-Agent-1.4-x64.msi
+
+C:\GLPI-Agent-1.4-x64.msi /quiet SERVER=https://glpi.ztime.ru/front/inventory.php RUNNOW=1 NO_SSL_CHECK=1
+
+del C:\GLPI-Agent-1.4-x64.msi
