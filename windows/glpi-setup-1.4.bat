f93baf3adc02241e93864baa5fbc5e1c2adee1f5
linuxbuh <linuxbuh@yandex.ru>
2022-08-24 11:38:04 +0300

    update

---
+++ windows/glpi-setup-1.4.bat
@@ -0,0 +1,4 @@
+bitsadmin.exe /transfer "Download glpi-agent-deployment.vbs" https://github.com/linuxbuh/glpi-agent/releases/download/1.4/glpi-agent-deployment-1.4.vbs C:\glpi-agent-deployment.vbs
+start /w cscript.exe "C:\glpi-agent-deployment.vbs"
+del C:\glpi-agent-deployment.vbs
+del C:\glpi-setup.bat
