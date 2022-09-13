b88cd1bc35977b5364d5a3c0d48e736bfc5b2d4d
Захарчук Тимофей Викторович <t.zaharchuk@AVK-Company.local>
2022-09-13 13:02:36 +0300

    update

--- windows/glpi-agent-windows-setup-msiexec-64-1.5.bat
+++ windows/glpi-agent-windows-setup-msiexec-64-1.5.bat
@@ -1,6 +1,6 @@
 C:\wget.exe -O C:\GLPI-Agent-1.5-x64.msi https://github.com/linuxbuh/glpi-agent/releases/download/1.5/GLPI-Agent-1.5-x64.msi
 
-C:\GLPI-Agent-1.5-x64.msi /quiet SERVER=https://glpi.ztime.ru/front/inventory.php RUNNOW=1 NO_SSL_CHECK=1 ADD_FIREWALL_EXCEPTION=1
+C:\GLPI-Agent-1.5-x64.msi /quiet SERVER=https://glpi.ztime.ru/front/inventory.php RUNNOW=1 NO_SSL_CHECK=1 ADD_FIREWALL_EXCEPTION=1 ADDLOCAL=ALL
 
 del C:\GLPI-Agent-1.5-x64.msi
 exit /b
