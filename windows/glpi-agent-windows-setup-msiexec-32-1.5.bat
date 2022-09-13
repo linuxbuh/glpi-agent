c8d93f42d7671892d252ead74efb01524891176e
Захарчук Тимофей Викторович <t.zaharchuk@AVK-Company.local>
2022-09-13 12:59:44 +0300

    update ADD_FIREWALL_EXCEPTION=1

--- windows/glpi-agent-windows-setup-msiexec-32-1.5.bat
+++ windows/glpi-agent-windows-setup-msiexec-32-1.5.bat
@@ -1,3 +1,3 @@
 C:\wget.exe  -O C:\GLPI-Agent-1.5-x86.msi https://github.com/linuxbuh/glpi-agent/releases/download/1.5/GLPI-Agent-1.5-x86.msi
-C:\GLPI-Agent-1.5-x86.msi /quiet SERVER=https://glpi.ztime.ru/front/inventory.php RUNNOW=1 NO_SSL_CHECK=1
+C:\GLPI-Agent-1.5-x86.msi /quiet SERVER=https://glpi.ztime.ru/front/inventory.php RUNNOW=1 NO_SSL_CHECK=1 ADD_FIREWALL_EXCEPTION=1
 exit /b
